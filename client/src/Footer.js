8738a78fc542b19c26f42de6052a9ac95ac9e3ff
Djinn Djinn <medi.cypo@gmail.com>
2021-08-26 10:53:48 +0100

    First commit

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        Decentralized Exchange
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
